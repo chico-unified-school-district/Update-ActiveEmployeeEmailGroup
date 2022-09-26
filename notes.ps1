94c6149768f67f355dcf905dd71b38d4c2ff3b26
Justin Cooper <jcooper@chicousd.org>
2022-09-26 13:25:39 -0700

    removed files

--- notes.ps1
+++
@@ -1,7 +0,0 @@
-$params = @{
- dcs                  = 'Mainframe.chico.usd', 'optimus.chico.usd', 'kickoff.chico.usd'
- ADCred               = $adTasks
- MonthsSinceLastLogon = 18
-}
-$params
-ls -Recurse -Filter *.ps1 | Unblock-File
